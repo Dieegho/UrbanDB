bd29484b7f155c8d0cde31a18f7b5b07e13fb86a
nicolaspoza <nicolas.poza@mail.udp.cl>
2021-05-09 23:50:51 -0400

    sin cambios

--- backend/app/main/util/dto.py
+++ backend/app/main/util/dto.py
@@ -4,11 +4,11 @@ from flask_restplus import Namespace, fields
 class UserDto:
     api = Namespace('user', description='user related operations')
     user = api.model('user', {
-        'id': fields.Integer(required=True, description='user id'),
+        # 'id': fields.Integer(required=True, description='user id'),
         'email': fields.String(required=True, description='user email address'),
         'username': fields.String(required=True, description='user username'),
         'password': fields.String(required=True, description='user password'),
-        'public_id': fields.String(description='user Identifier')
+        # 'public_id': fields.String(description='user Identifier')
     })
 
 
