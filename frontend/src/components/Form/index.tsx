a9fa07c79d9250f9b6fc92cf1234ee888040f372
= <=pamela.pinilla98@gmail.com>
2021-05-10 22:02:09 -0400

    ingresar y retirar se autorellenan con la pistola, falta ocultar el campo id

--- frontend/src/components/Form/index.tsx
+++ frontend/src/components/Form/index.tsx
@@ -1,30 +1,34 @@
-import React, { useState, FC, useRef } from 'react';
-//import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import React, { useState, FC, useRef, useEffect } from 'react';
+import axios from 'axios';
 import Form from 'react-bootstrap/Form';
 import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
-import axios from 'axios';
 
 interface props {
   handleAddItemsTable ?: (item) => void;
   handleRetirarItems ?: (item) => void;
   handleLoginUsers ?: (user) => void;
   handleAddNewItemsTable ?: (item) => void;
+  items_id:{
+    id:number;
+    codigo: string;
+    nombre: string;
+    unidad_medida: string;
+  }[];
 }
 
-const MyForm: FC<props> = ({ handleAddItemsTable, handleRetirarItems, handleLoginUsers, handleAddNewItemsTable}) => {
-
+const MyForm: FC<props> = ({ handleAddItemsTable, handleRetirarItems, handleLoginUsers, handleAddNewItemsTable, items_id}) => {
+  
   if(handleAddItemsTable){
+
     const [scannerid, setScannerId] = useState("")
     const [codigo, setCodigo] = useState("");
     const [name, setName] = useState("");
     const [unidadMedida, setUnidadMedida] = useState("");
-    const [critico, setCritico] = useState("");
     const [cantidad, setCantidad] = useState("");
-    const [id_categoria, setId_categoria] = useState("");
     const [validated, setValidated] = useState(false);
     const idScannerRef = useRef(null);
-
+    
     const handleSubmit = (e) =>{
       const form = e.currentTarget;
 
@@ -38,41 +42,28 @@ const MyForm: FC<props> = ({ handleAddItemsTable, handleRetirarItems, handleLogi
         codigo: codigo,
         nombre: name,
         unidad_medida: unidadMedida,
-        critico : critico,
         cantidad: cantidad,
-        id_categoria: id_categoria,
       }
 
       handleAddItemsTable(data);
-      axios.post('http://127.0.0.1:5000/item/', {codigo, name, unidadMedida, critico, cantidad, id_categoria} )
+      axios.post('http://127.0.0.1:5000/item/', {codigo, name, unidadMedida, cantidad} )
       .then(res => {
         console.log(res);
       })
     }
 
     const handleIdScanner = (e) => {
-      const id = e.target.value;
+      let id = e.target.value;
       console.log(id);
       setScannerId(id);
       
-      const data = {
-        codigo: codigo,
-        nombre: name,
-        unidad_medida: unidadMedida,
-        critico: critico,
-        id_categoria: id_categoria,
-      }
-
-      axios.get(`http://127.0.0.1:5000/item/pistola/${id}`)
-      .then(res => {
-        console.log(res);
+      items_id.map((elem)=>{
+        if(id == elem.id){
+          setCodigo(elem.codigo);
+          setName(elem.nombre);
+          setUnidadMedida(elem.unidad_medida);
+        }
       })
-
-      setCodigo(data.codigo)
-      setName(data.nombre)
-      setUnidadMedida(data.unidad_medida)
-      setCritico(data.critico)
-      setId_categoria(data.id_categoria)
     }
 
     return(
@@ -120,30 +111,6 @@ const MyForm: FC<props> = ({ handleAddItemsTable, handleRetirarItems, handleLogi
           </Form.Row>
 
           <Form.Row>
-          <Form.Group as={Col} md="4" controlId="id_categoria">
-            <Form.Label>ID Categoría</Form.Label>
-              <Form.Control
-                required type="number" 
-                value={id_categoria}
-                min={1}
-                onChange={(e) => setId_categoria(e.target.value)}
-              />
-              <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
-            </Form.Group>
-
-            <Form.Group as={Col} md="4" controlId="critico">
-              <Form.Label>Stock crítico</Form.Label>
-              <Form.Control
-                required 
-                type="number" 
-                min={0}
-                
-                value={critico}
-                onChange={(e) => setCritico(e.target.value)}
-              />
-              <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
-            </Form.Group>
-
             <Form.Group as={Col} md="4" controlId="cantidad">
               <Form.Label>Cantidad de Productos</Form.Label>
               <Form.Control
@@ -161,7 +128,7 @@ const MyForm: FC<props> = ({ handleAddItemsTable, handleRetirarItems, handleLogi
             <Form.Group as={Col} md="4" controlId="id_scanner">
               <Form.Control
                 required
-                hidden={true}
+                hidden={false}
                 ref={idScannerRef}
                 type="number" 
                 value={scannerid}
@@ -213,20 +180,17 @@ const MyForm: FC<props> = ({ handleAddItemsTable, handleRetirarItems, handleLogi
     }
 
     const handleIdScanner = (e) => {
-      const id = e.target.value;
+      let id = e.target.value;
       console.log(id);
       setScannerId(id);
       
-      //llamada a backend
-
-      const data = {
-        codigo: "codigo",
-        nombre: "name",
-        unidad_medida: (id % 2 === 0 ? "kit":"UN") ,
-      }
-      setCodigo(data.codigo)
-      setName(data.nombre)
-      setUnidadMedida(data.unidad_medida)
+      items_id.map((elem)=>{
+        if(id == elem.id){
+          setCodigo(elem.codigo);
+          setName(elem.nombre);
+          setUnidadMedida(elem.unidad_medida);
+        }
+      })
     }
 
     return(
@@ -242,17 +206,6 @@ const MyForm: FC<props> = ({ handleAddItemsTable, handleRetirarItems, handleLogi
               <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
             </Form.Group>
 
-            <Form.Group as={Col} controlId="cantidad">
-              <Form.Label>Cantidad</Form.Label>
-              <Form.Control
-                value={cantidad}
-                onChange={(e) => setCantidad(e.target.value)}
-              />
-              <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
-            </Form.Group>
-
-          </Form.Row>
-          <Form.Row>
             <Form.Group as={Col} controlId="nombre">
               <Form.Label>Nombre</Form.Label>
               <Form.Control
@@ -260,6 +213,17 @@ const MyForm: FC<props> = ({ handleAddItemsTable, handleRetirarItems, handleLogi
                 placeholder="Ingrese el nombre del producto"
                 value={name}
                 onChange={(e) => setName(e.target.value)}
+              />
+              <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+            </Form.Group>
+          </Form.Row>
+          <Form.Row>
+
+            <Form.Group as={Col} controlId="cantidad">
+              <Form.Label>Cantidad</Form.Label>
+              <Form.Control
+                value={cantidad}
+                onChange={(e) => setCantidad(e.target.value)}
               />
               <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
             </Form.Group>
@@ -284,7 +248,7 @@ const MyForm: FC<props> = ({ handleAddItemsTable, handleRetirarItems, handleLogi
             <Form.Group as={Col} md="4" controlId="id_scanner">
               <Form.Control
                 required
-                hidden={true}
+                hidden={false}
                 ref={idScannerRef}
                 type="number" 
                 value={scannerid}
@@ -293,7 +257,7 @@ const MyForm: FC<props> = ({ handleAddItemsTable, handleRetirarItems, handleLogi
             </Form.Group>
           </Form.Row>
 
-          <Button variant="outline-dark" onClick = {(e) => idScannerRef.current.focus()}>
+          <Button variant="outline-dark" type="submit">
             Retirar
           </Button>
 
